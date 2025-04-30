ea765da4b56b0f23382ff6ec0784db5d11992fa1
Nitro747 <gustavo.sanches.silva@gmail.com>
2025-04-30 20:30:35 -0300

    finished project, will come back for better styling later

--- amazon-backend-scraper/index.ts
+++ amazon-backend-scraper/index.ts
@@ -1,13 +1,82 @@
-console.log("Hello via Bun!"); 
+import axios from "axios";
+import express from "express";
+import { JSDOM } from "jsdom";
+import cors from "cors";
 
-import figlet from "figlet";
+const app = express();
+const port = 3000;
 
-const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-        const body = figlet.textSync("Bun!");
-        return new Response(body);
-    },
-  });
-  
-  console.log(`Listening on http://localhost:${server.port} ...`);
+app.use(cors());/* enable cross origin */
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World!");
+});
+
+/* Fetching HTML data from AMZ */
+const beginScrape = async (product: string) => {
+  try {
+    const res = await axios.get(`https://www.amazon.com/s?k=${product}`, {
+      headers: {
+        /* User agents need to be commented in and out in rotation in order to prevent Amazon blocks */
+        "User-Agent":
+        /* Mozilla/5.0 (Linux; Android 13; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.207 Mobile Safari/537.36 */
+        /*   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36" */
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+        "Accept-Language": "en-US,en;q=0.9",
+      },
+    })
+    /* Parsing HTML into JSDOM to select the products name, rating, etc */
+    const dom = new JSDOM(res.data);
+    const document = dom.window.document;
+    const productCard = document.querySelectorAll("div[data-component-type='s-search-result']")
+    const products: any[] = []  /* Array of products */
+
+    productCard.forEach((card: any) => {
+      const sponsored = card.querySelector("span[data-component-type='sp-sponsored-result']");
+      if (sponsored) return; /* to skip sponsored ads */
+      const badge = card.querySelector("span.sponsored-label-text");
+      if (badge) return; /* to skip sponsored ads */
+
+      const titleHtml = card.querySelector("h2 > a > span") || card.querySelector("h2 span");
+      const ratingHtml = card.querySelector("span.a-icon-alt");
+      const reviewHtml = card.querySelector("span[aria-label*='rating'] ~ span.a-size-base");
+      const imageHtml = card.querySelector("img.s-image"); /* selecting titles, ratings, etc */
+
+      const title = titleHtml?.textContent?.trim();
+      const rating = ratingHtml?.textContent?.trim();
+      const review = reviewHtml?.textContent?.trim();
+      const image = imageHtml?.src; /* selecting only the strings */
+
+      if (title && /\w+/.test(title)) { /* filtering out white space + unknown characters */
+        products.push({title, rating, review, image});
+      }
+    });
+    return products; 
+
+  } catch (e) {
+    console.log("Error", e)
+  }
+}
+
+/* Running the request */
+app.get("/api/scrape", async (req: any, res: any) => {
+  const keyword = req.query.keyword;
+  console.log(`Received request for keyword: ${keyword}`); /* testing */
+  if (!keyword) {
+    return res.status(400).json({ error: "Empty field!" });
+  }
+
+  try {
+    const productsFinal = await beginScrape(keyword);
+    console.log(`Products fetched for ${keyword}:`, productsFinal); /* testing */
+    res.json({ keyword, count: productsFinal?.length, products: productsFinal })
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+
+/* Confirmation code of listening on port 3000 */
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
--- amazon-vanilla-scraper/src/counter.js
+++ amazon-vanilla-scraper/src/counter.js
@@ -1,9 +1,43 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+export function beginScrape(element) {
+  const input = document.querySelector(".input"); /* selects the input section */
+  const resultsContainer = document.createElement("div");
+  resultsContainer.className = "results"
+  const appDiv = document.querySelector("#app")
+  appDiv.appendChild(resultsContainer) /* appends the search results onto the DOM */
+ /*  document.body.appendChild(resultsContainer); */ 
+
+  const beginScrape = async () => {
+    const keyword = input.value.trim();
+    if (!keyword) {
+      alert("Input field is empty!");
+      return;
+    }
+    try {
+      const response = await fetch(`http://localhost:3000/api/scrape?keyword=${encodeURIComponent(keyword)}`); /* calling the backend and passing the keyword */
+      const data = await response.json();
+      console.log(data);
+  
+      /* inserting the results on the front end */
+      resultsContainer.innerHTML = 
+      `<h2> Results for "${keyword}" (${data.count || 0} items) </h2>
+          
+          <ul>
+            ${(data.products || []) 
+              .filter(item => item && typeof item === "object" || undefined) /* filtering out nul results + looping over each item to append a new div */
+              .map(item => 
+                `<li>
+                <img src="${item.image}" alt="Product image" width="100" />
+                <strong>${item.title}</strong><br />
+                Rating: ${item.rating || "N/A"}<br />
+                Reviews: ${item.review || "N/A"}<br />
+                
+              </li>`).join("")}
+          </ul>`;
+          
+    } catch (error) {
+      console.error("Error fetching the data", error);
+      resultsContainer.innerHTML = `<p>Failed to load data!</p>`
+    }
+  };
+  element.addEventListener('click', beginScrape)
 }
